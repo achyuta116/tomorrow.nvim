ad53dde9e7d1fb91e17d49dc44123a87bbc91a11
Achyuta <achyutathegreat@gmail.com>
2024-01-08 22:21:32 +0530

    init: file creation

--- lua/tomorrow/init.lua
+++ lua/tomorrow/init.lua
@@ -1,1 +1,29 @@
-return 7
+-- when reopening neovim, present the
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+-- user with a prompt to specify valid filepath for file which has been moved
+
+-- our picker function: colors
+
+-- to execute the function
+local file, err = io.open(vim.fn.stdpath('data') .. "/tomorrow.json", "r+")
+if file then
+
+else
+    file, err = io.open(vim.fn.stdpath('data') .. "/tomorrow.json", "w")
+    if file then
+        file:write("[]")
+    else
+        print("Error Creating tomorrow.json", err)
+    end
+end
+
+
+-- a table which stores a list of (comment) for a given filename
+-- a function which adds a (file, comment) to a table
+-- a function which deletes the (file, comment) from the table
+-- persist the table in memory somehow
+-- before completion of renaming, prompt the user
