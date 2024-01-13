05817191a233feb5c2ad0ca8f3b8d7a8f7d3f4ff
Achyuta <achyutathegreat@gmail.com>
2024-01-13 22:31:54 +0530

    fix: json writing bug fixed, added config option

--- lua/tomorrow/init.lua
+++ lua/tomorrow/init.lua
@@ -14,57 +14,50 @@ local M = {}
 M.files = {}
 
 -- persist the table in memory somehow
-local _persist = function()
-    local file, err = io.open(vim.fn.stdpath('data') .. "/tomorrow.json", "w+")
+local function _persist()
+    local file, err = io.open(vim.fn.stdpath('data') .. "/tomorrow.json", "w")
     local new_content = vim.json.encode(M.files)
+    P(new_content .. "\n")
     if file then
         file:write(new_content)
+        file:close()
     else
         print("Error Writing to tomorrow.json: ", err)
     end
 end
 
-local _init = function()
-    local file, err = io.open(vim.fn.stdpath('data') .. "/tomorrow.json", "r+")
-    if file then
-        local content = file:read("*a")
-        local ok, result = pcall(vim.json.decode, content)
-        if ok then
-            M.files = result
-        else
-            print("Error Parsing tomorrow.json", result)
-        end
-    else
-        file, err = io.open(vim.fn.stdpath('data') .. "/tomorrow.json", "w")
-        if file then
-            file:write("[]")
-        else
-            print("Error Creating tomorrow.json", err)
-        end
-    end
+local function _init()
 end
 
 -- a function which adds a (file, comment) to a table
-local _add_to_files = function(path, comment)
+local function _add_to_files(path, comment)
+    local to_insert = { comment = comment, time = os.date("%Y-%m-%d %H:%M:%S", os.time()) }
+
     if not M.files[path] then
         M.files[path] = {
-            { comment = comment, time = os.date("%Y-%m-%d %H:%M:%S", os.time()) }
+            to_insert
         }
     else
-        table.insert(M.files[path], comment)
+        table.insert(M.files[path], to_insert)
     end
     _persist()
 end
 
-M.add_comment = function()
+function M.add_comment()
     _add_to_files(vim.fn.expand("%:p"), vim.fn.input("Add comment: "))
 end
 
-
 -- a function which deletes the (file, comment) from the table
-local _delete_from_file = function(path, comment)
+local function _delete_from_file(path, comment)
     local index = nil
-    for idx, file_comment in ipairs(M.files[path]) do
+    local comments = M.files[path]
+    if not comments then
+        print("No comments for this file.")
+        return
+    end
+
+    for idx, file_comment in ipairs(comments) do
+        P(file_comment)
         if file_comment.comment == comment then
             index = idx
             break
@@ -76,13 +69,24 @@ local _delete_from_file = function(path, comment)
     end
 end
 
-M.delete_comment = function()
+function M.delete_comment()
+    P(M.files[vim.fn.expand("%:p")])
+
     pickers.new(require("telescope.themes").get_dropdown {}, {
         prompt_title = "Delete Comment",
         finder = finders.new_table {
-            results = M.files[vim.fn.expand("%:p")]
+            results = M.files[vim.fn.expand("%:p")],
+            entry_maker = function(entry)
+                print("From Entry_maker\n")
+                P(entry)
+                return {
+                    value = entry.comment,
+                    display = entry.comment,
+                    ordinal = entry.time
+                }
+            end
         },
-        attach_mappings = function(prompt_bufnr, map)
+        attach_mappings = function(prompt_bufnr, _)
             actions.select_default:replace(function()
                 actions.close(prompt_bufnr)
                 local selection = action_state.get_selected_entry()
@@ -95,14 +99,13 @@ M.delete_comment = function()
     _persist()
 end
 
-
 -- before completion of renaming, prompt the user
 
-local _get_root = function()
+local function _get_root()
     return vim.loop.cwd()
 end
 
-local _get_relevant_files = function()
+local function _get_relevant_files()
     local filenames = {}
     for filename, _ in pairs(M.files) do
         if filename:sub(1, #_get_root()) == _get_root() then
@@ -113,11 +116,11 @@ local _get_relevant_files = function()
     return filenames
 end
 
-local _get_comments = function(filename)
+local function _get_comments(filename)
     return M.files[filename]
 end
 
-M.list_comments = function()
+function M.list_comments()
     local comments = _get_comments(vim.fn.expand("%:p"))
     if not comments or vim.tbl_count(comments) == 0 then
         print("No comments available for this file")
@@ -137,10 +140,37 @@ M.list_comments = function()
     end
 end
 
-vim.api.nvim_set_keymap("n", "<leader>ta", ":lua require('tomorrow').add_comment()<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>tx", ":lua require('tomorrow').delete_comment()<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>tl", ":lua require('tomorrow').list_comments()<CR>", {})
+function M.setup(config)
+    local file, err = io.open(vim.fn.stdpath('data') .. "/tomorrow.json", "r")
+    if file then
+        local content = file:read("*a")
+        local ok, result = pcall(vim.json.decode, content)
+        if ok then
+            M.files = result
+        else
+            print("Error Parsing tomorrow.json", result, content)
+        end
+        file:close()
+    else
+        file, err = io.open(vim.fn.stdpath('data') .. "/tomorrow.json", "w")
+        if file then
+            file:write("[]")
+            file:close()
+        else
+            print("Error Creating tomorrow.json", err)
+        end
+    end
 
-_init()
+    if config.default then
+        function config.on_attach(bufnr)
+            vim.api.nvim_set_keymap("n", "<leader>tx", ":lua require('tomorrow').delete_comment()<CR>", {})
+            vim.api.nvim_set_keymap("n", "<leader>ta", ":lua require('tomorrow').add_comment()<CR>", {})
+            vim.api.nvim_set_keymap("n", "<leader>tl", ":lua require('tomorrow').list_comments()<CR>", {})
+        end
+    end
+
+    print("config setup")
+    config.on_attach()
+end
 
 return M
